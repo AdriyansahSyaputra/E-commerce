b515ac344279d7c768c83d07ccbbbeedfbb4b2ca
Adriyansah Syaputra <adriansyahputra2626@gmail.com>
2025-01-15 23:26:38 +0700

    add promo banner at the product page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Product from './pages/Product'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/about' element={<About />}></Route>
+          <Route path='/product' element={<Product />}></Route>
         </Routes>
       </Router>
     </>
---
+++ src/pages/Product.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "../components/template/navbar/Navbar";
+import Footer from "../components/template/footer/footer";
+import PromoBanner from "../components/Layouts/Product/PromoBanner";
+
+const Product = () => {
+    return (
+        <>
+        <Navbar />
+        <PromoBanner />
+        <main>
+            
+        </main>
+        <Footer />
+        </>
+    )
+}
+
+export default Product;
